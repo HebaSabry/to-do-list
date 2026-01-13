ff0e88a737230cdf98c20d57765d9f5683a5c04c
HebaSabry <hebasabryeldwaik@gmail.com>
2026-01-13 21:41:46 +0200

    Refactor task addition and storage logic

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
 main.js
-  function addTask(){
-    const input =document.getElementById('taskInput');
-  const taskText =input.value.trim();
- if(taskText ==='')return
- const li =document.createElement('li');
+ 
+ function createTask(taskText)
+ {const li =document.createElement('li')
  li.textContent =taskText;
+
  li.onclick =function(){
      li.classList.toggle('completed');
  };
  document.getElementById('taskList').appendChild(li);
-  input.value ='';
- 
 }
+  
+ 
+
+
+
+
+function addTask(){
+    const input =document.getElementById('taskInput');
+  const taskText =input.value.trim();
+ if(taskText ==='')return
+ createTask(taskText);
+ const tasks =JSON.parse(localStorage.getItem('tasks')) ||[];
+ tasks.push(taskText);
+ localStorage.setItem('tasks',JSON.stringify(tasks));
+ input.value ='';
+}
+window.onload =function(){
+    const savedTasks =JSON.parse(localStorage.getItem('tasks')) ||[];
+    savedTasks.forEach(taskText =>createTask(taskText));
+}
+ 
